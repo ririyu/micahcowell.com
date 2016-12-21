2bae822afbbdb564adb3449b0b384c59d30f967a
Micah Cowell <micahcowell@gmail.com>
2016-12-21 03:40:34 -0800

    smooth appearance on data load

--- src/Components/Music/index.js
+++ src/Components/Music/index.js
@@ -5,8 +5,9 @@ import Icon from './icon.png';
 const getRecentTracksURL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=getmicah&api_key=b4678efadfadbcf8882ed8e5432173b1&format=json";
 
 export default class Music extends React.Component {
+
 	state = {
-		recentTrack: ''
+		response: 'John Cena'
 	}
 
 	loadTrackData() {
@@ -14,9 +15,9 @@ export default class Music extends React.Component {
 			if (!error && response.statusCode === 200) {
 				var track = JSON.parse(body).recenttracks.track[0];
 				var data =  track.artist['#text'] + ' - ' + track.name;
-				this.setState({recentTrack:data});
+				this.setState({response:data});
 			} else {
-
+				this.setState({response:'Error: could\'t connect to last.fm'});
 			}
 		}.bind(this));
 	}
@@ -26,12 +27,20 @@ export default class Music extends React.Component {
 	}
 
 	render() {
+		const textStyle = {
+			opacity: 0
+		}
+		if (this.state.response !== 'John Cena') {
+			textStyle.opacity = 1;
+		}
 		return (
 			<div>
 				<img src={Icon} alt="headphone" />
 				<p>
-					<a href="https://last.fm/user/getmicah" ref="track">
-						{this.state.recentTrack}
+					<a className="data"
+					   style={textStyle}
+					   href="http://last.fm/user/getmicah">
+						{this.state.response}
 					</a>
 				</p>
 			</div>
