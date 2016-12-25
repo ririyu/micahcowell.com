78ede8cbc1cde087a7d6b7f68e3fcacef6cc62e2
Micah Cowell <micahcowell@gmail.com>
2016-12-25 00:46:29 -0800

    es6 looks so nice

---
+++ src/Components/game.js
@@ -0,0 +1,10 @@
+// imports
+
+export default class Game {
+	constructor(ctx) {
+		this.ctx = ctx;
+	}
+	test() {
+		console.log('TEST!');
+	}
+}
--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
-const app = document.querySelector('#app');
-app.innerHTML = 'suc';
+import css from './Styles/main.scss';
+import Game from './Components/game.js';
+
+// create elements
+const app = document.getElementById('app');
+const container = document.createElement('div');
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+container.id = 'app-container';
+canvas.width = 240;
+canvas.height = 160;
+app.appendChild(container).appendChild(canvas);
+
+// configure game
+const game = new Game(ctx);
+game.test();
