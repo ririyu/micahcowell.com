6195392680da405b7a86497a6614676f82cb5183
Micah Cowell <micahcowell@gmail.com>
2017-01-08 20:46:23 -0800

    starry night is too bugy

--- src/app.js
+++ src/app.js
@@ -3,10 +3,6 @@ require('../scss/main.scss');
 import CanvasGame from './CanvasGame';
 import LastFmGrabber from './LastFmGrabber';
 
-// background canvas effect
-let starryNight = new CanvasGame('#starry-night');
-starryNight.init();
-
 // now playing from last.fm
 let nowPlaying = new LastFmGrabber('#now-playing');
 nowPlaying.fetch();
