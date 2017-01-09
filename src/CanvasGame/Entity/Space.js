ece05acfba4c689ceb69a2ade0610f3aaf79223f
Micah Cowell <micahcowell@gmail.com>
2017-01-08 16:22:40 -0800

    starlord > starboy

--- src/CanvasGame/Entity/Space.js
+++ src/CanvasGame/Entity/Space.js
@@ -5,16 +5,19 @@ export default class Space extends Entity {
 		super(ctx);
 		this.v = 1;
 		this.stars = [];
-		this.complexity = 1000;
+		this.multiplier = 0.001; // bigger = more stars
 	}
 	init(x, y) {
 		super.init(x, y);
 		this.build();
 	}
 	build() {
-		for (let i = 0; i < this.complexity; i++) {
-			let x = Math.random() * window.screen.width;
-			let y = Math.random() * window.screen.height;
+		let sw = window.screen.width;
+		let sh = window.screen.height;
+		let starCount = this.multiplier * sw * sh;
+		for (let i = 0; i < (starCount); i++) {
+			let x = Math.random() * sw;
+			let y = Math.random() * sh;
 			let w = Math.random() * (5 - 2) + 2;
 			this.stars[i] = {x, y, w};
 		}
